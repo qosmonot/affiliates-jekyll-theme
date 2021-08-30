114cf46b7d58120782aba41892989615267a9dd8
qosmonot <32738380+qosmonot@users.noreply.github.com>
2021-08-31 01:34:29 +0300

    test js edit

--- blog/assets/js/theme.js
+++ blog/assets/js/theme.js
@@ -1,11 +1,11 @@
 
       (function ($) {"use strict";
-      
+
       $(function () {
         var header = $(".start-style");
         $(window).scroll(function () {
           var scroll = $(window).scrollTop();
-    
+
           if (scroll >= 10) {
             header.removeClass('start-style').addClass("scroll-on");
           } else {
@@ -13,15 +13,15 @@
           }
         });
       });
-    
+
       //Animation
-    
+
       $(document).ready(function () {
         $('body.hero-anime').removeClass('hero-anime');
       });
-    
+
       //Menu On Hover
-    
+
       $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
         if ($(window).width() > 750) {
           var _d = $(e.target).closest('.nav-item');_d.addClass('show');
@@ -30,12 +30,12 @@
           }, 1);
         }
       });
-    
-    
-    
+
+
+
     })(jQuery);
 
 // Blank Target External Links
-$(document.links).filter(function() {
-return this.hostname != window.location.hostname;
-}).attr('target', '_blank');
+// $(document.links).filter(function() {
+// return this.hostname != window.location.hostname;
+// }).attr('target', '_blank');
